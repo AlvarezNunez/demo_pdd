519d9af9d2ee5d26ca6715518af29bf030550989
AlvarezNunez <41631486+AlvarezNunez@users.noreply.github.com>
2021-07-14 15:50:41 +0300

    Create 2.java

---
+++ 2.java
@@ -0,0 +1,11 @@
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        // Prints "Hello, World" to the terminal window.
+        System.out.println("Hello, World");
+    }
+// @todo Need method Alfa (do the best!)
+// @todo Need method Beta (do the fast!)
+// @todo Need method Gamma (do the quality!)
+  
+}
